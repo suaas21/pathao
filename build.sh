e46d640ba3bb6f7b04909aca210a4087601d3a90
Sayf Uddin Al Azad Sagor <suaas@Sayfs-MacBook-Pro.local>
2022-03-24 17:28:08 +0600

    add comment to build.sh file

--- build.sh
+++ build.sh
@@ -5,14 +5,22 @@ PROJ="pathao" # your repo name
 ORG_PATH="github.com/suaas21"
 REPO_PATH="${ORG_PATH}/${PROJ}"
 
+echo "checking go installed"
+
 if ! [ -x "$(command -v go)" ]; then
     echo "go is not installed"
     exit 1
 fi
+
+echo "checking git installed"
+
 if ! [ -x "$(command -v git)" ]; then
     echo "git is not installed"
     exit 1
 fi
+
+echo "set GOPATH"
+
 if [ -z "${GOPATH}" ]; then
     echo "set GOPATH"
     exit 1
@@ -32,6 +40,8 @@ fi
 if [ -n "$(git diff --shortstat 2> /dev/null | tail -n1)" ]; then
     VERSION="${VERSION}-dirty"
 fi
+
+echo "go installing....."
 
 export GO111MODULE=on
 
